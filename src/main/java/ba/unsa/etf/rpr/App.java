f3f38da036a236b0a3d97ddb610639c10c23b455
Anida Nezovic <anezovic1@etf.unsa.ba>
2022-10-30 10:22:22 +0100

    new comment

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -2,12 +2,13 @@ package ba.unsa.etf.rpr;
 
 /**
  * Hello world!
- *
+ * comment by anida
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        System.out.println( "Zdravo, svijete" );
     }
 }
